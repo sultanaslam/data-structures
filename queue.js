d3122de1807312b4fabbc9de5fa8e0580edfbbfe
root <root@ICB21.localdomain>
2017-12-17 16:22:05 -0800

    MSG

--- queue.js
+++ queue.js
@@ -1,4 +1,23 @@
 function Queue() {
 
   this.storage = [];
+
 };
+
+Queue.prototype.add = function(value) {
+	this.storage.push(value);
+}
+
+Queue.prototype.remove = function() {
+	if (this.storage.length) {
+		return this.storage.shift();
+	}else{
+		console.log('Queue is Empty.');
+	}
+}
+
+Queue.prototype.size = function() {
+	return this.storage.length;
+}
+
+var que = new Queue();
--- stack.js
+++ stack.js
@@ -1,5 +1,21 @@
 function Stack() {
 
   var storage = [];
+  this.add = function(value) {
+		storage.push(value);
+	}
 
-}
+	this.remove = function() {
+		if (storage.length) {
+			return storage.pop();
+		}else{
+			console.log('Stack is Empty.');
+		}
+	}
+
+	this.size = function() {
+		return storage.length;
+	}
+};
+
+var stack = new Stack();
